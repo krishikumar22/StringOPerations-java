b127c3b49bed0008b1c87aca9274c02833986959
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-26 20:01:49 +0530

    Add files via upload

---
+++ Stringex1.java
@@ -0,0 +1,14 @@
+
+public class Stringex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuffer sb1=new StringBuffer("Krishi");
+StringBuffer sb2=new StringBuffer("Krishi");
+System.out.println(sb1==sb2);
+System.out.println(sb1.equals(sb2));
+
+
+	}
+
+}
