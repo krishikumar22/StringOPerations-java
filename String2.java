0888b89a6f71030243e64d1c206ca8fa5601b528
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-26 20:02:07 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,12 @@
+
+public class String2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1=new String("krishi");
+String s2=new String("krishi");
+System.out.println(s1==s2);
+System.out.println(s1.equals(s2));
+	}
+
+}
