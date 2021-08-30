091ea5c5d4bedea082f146266d0b7f5cff9844b3
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-30 18:57:48 +0530

    Add files via upload

---
+++ StrCharAt1.java
@@ -0,0 +1,11 @@
+
+public class StrCharAt1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="Krishi";
+char ch=name.charAt(3);
+System.out.println(ch);
+	}
+
+}
