a853d8f039077c120ea5932540c201761313927b
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-30 18:58:26 +0530

    Add files via upload

---
+++ StrChex3.java
@@ -0,0 +1,16 @@
+
+public class StrChex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="Welcome to string programming";
+int Stringlength=str.length();
+
+for(int i=0;i<=str.length()-1;i++)
+	if(i%2!=0) {
+		System.out.println(str.charAt(i));
+		
+	}
+	}
+
+}
