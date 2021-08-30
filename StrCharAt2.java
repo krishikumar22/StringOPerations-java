04b0a04c719f22611aed220abd6e532865f02991
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-30 18:58:06 +0530

    Add files via upload

---
+++ StrCharAt2.java
@@ -0,0 +1,14 @@
+
+public class StrCharAt2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="Welcome to String programming";
+int Stringlength=str.length();
+char a=str.charAt(0);
+char b=str.charAt(str.length()-1);
+System.out.println("The first character is:"+a);
+System.out.println("The last character is:"+b);
+	}
+
+}
