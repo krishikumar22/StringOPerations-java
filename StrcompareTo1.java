9b3a68dc3960a2838e0aab1ef7247dd4a131e38a
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-30 19:29:09 +0530

    Add files via upload

---
+++ StrcompareTo1.java
@@ -0,0 +1,16 @@
+
+public class StrcompareTo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="hello";
+String s2="hello";
+String s3="hi";
+String s4="basicss";
+String s5="tell";
+System.out.println(s1.compareTo(s2));
+System.out.println(s2.compareTo(s5));
+System.out.println(s4.compareTo(s2));
+	}
+
+}
